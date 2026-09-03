6308a04f4e17578739bdad31ca901bfad831288a
Xiaoqing <xiaoqing.zhou@outlook.fr>
2026-09-03 12:24:45 +0200

    Remove read.py

--- read.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-df = pd.read_csv(r"C:\Projets_Data\ProjetFinal\vente\Person.csv",
-                  sep=r"\+\|", header=None, engine="python", nrows=5)
-print(df.shape)
-print(df)
